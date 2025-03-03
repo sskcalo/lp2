808e0c28a4fb286e9138e7b56105d11eb2073f50
sskcalo <heuReuxlApin.1205@gmail.com>
2025-03-03 22:08:05 +0900

    2回目のコミット

--- assets/script/js/slider.js
+++ assets/script/js/slider.js
@@ -1,35 +1,23 @@
 $(function () {
-    if (window.matchMedia('(max-width: 767px)').matches) {
-        $(".slider")
-            .slick({
-                autoplay: false,
-                arrows: true,
-                prevArrow: '<div class="slide-arrow prev-arrow"><i class="fas fa-arrow-left"></i></div>',
-                nextArrow: '<div class="slide-arrow next-arrow"><i class="fas fa-arrow-right"></i></div>',
-                dots: true,
-                dotsClass: "slider-dots",
-                fade: false,
-                infinite: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-
-            })
-    }
-
-    else if (window.matchMedia('(min-width:768px)').matches) {
-        $(".slider")
-            .slick({
-                autoplay: false,
-                arrows: true,
-                prevArrow: '<div class="slide-arrow prev-arrow"><i class="fas fa-arrow-left"></i></div>',
-                nextArrow: '<div class="slide-arrow next-arrow"><i class="fas fa-arrow-right"></i></div>',
-                dots: true,
-                dotsClass: "slider-dots",
-                fade: false,
-                infinite: true,
-                slidesToShow: 3,
-                slidesToScroll: 3,
-            })
-    }
-})
-
+    $(".slider").slick({
+        autoplay: false,
+        arrows: true,
+        prevArrow: '<div class="slide-arrow prev-arrow"><i class="fas fa-arrow-left"></i></div>',
+        nextArrow: '<div class="slide-arrow next-arrow"><i class="fas fa-arrow-right"></i></div>',
+        dots: true,
+        dotsClass: "slider-dots",
+        fade: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 768, // 399px以下のサイズに適用
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                },
+            },
+        ],
+    });
+});
